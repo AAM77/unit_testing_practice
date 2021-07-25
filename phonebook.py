900f39c1276d0e22681986e8ae8eba3b715cdfc2
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-07-25 19:38:25 -0400

    Add method that checks for name duplication.

--- phonebook.py
+++ phonebook.py
@@ -20,3 +20,14 @@ class PhoneBook():
 
     def lookup(self, name: str) -> str:
         return self.numbers[name]
+
+    def is_consistent(self, consistency: bool = True) -> bool:
+        phonebook_is_consistent: bool = consistency
+
+        for i, number in enumerate(self.numbers.values()):
+            for j, prefix in enumerate(self.numbers.values()):
+                if i != j and prefix in number:
+                    phonebook_is_consistent = False
+                    break
+
+        return phonebook_is_consistent
