c266b47c6a845a9b4f08d84a2867f7940347c9eb
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-07-25 19:38:01 -0400

    Create fixture to reduce code smell and add tests for determining consistency.

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -2,47 +2,54 @@ import inspect
 import pytest
 import phonebook
 
-def test_phonebook_exists():
-    assert phonebook.PhoneBook
+@pytest.fixture
+def phone_book():
+    return phonebook.PhoneBook()
+
+def test_phonebook_exists(phone_book: object):
+    assert phone_book
 
 def test_phonebook_type():
     assert inspect.isclass(phonebook.PhoneBook)
 
-def tests_phonebook_numbers_type():
-    new_phonebook = phonebook.PhoneBook()
-    assert type(new_phonebook.numbers) == dict
+def tests_phonebook_numbers_type(phone_book: object):
+    assert type(phone_book.numbers) == dict
 
-def test_phonebook_add():
-    new_phonebook: object = phonebook.PhoneBook()
-    new_phonebook.add(name="John Doe", number="1234567890")
-    assert isinstance(new_phonebook, phonebook.PhoneBook)
+def test_phonebook_add(phone_book: object):
+    phone_book.add(name="John Doe", number="1234567890")
+    assert isinstance(phone_book, phonebook.PhoneBook)
 
-def test_add_multiple():
+def test_add_multiple(phone_book: object):
     people: list = [
         ("f1 l1", "198724"),
         ("f2 l2", "298734")
     ]
 
-    new_phonebook: object = phonebook.PhoneBook()
-    assert new_phonebook.add_multiple(people) == {
+    phone_book.add_multiple(people)
+
+    assert phone_book.numbers == {
                                                     "f1 l1": "198724",
                                                     "f2 l2": "298734"
                                                 }
 
-def test_phonebook_lookup():
-    new_phonebook: object = phonebook.PhoneBook()
-    new_phonebook.add(name="John Doe", number="1234567890")
-    number = new_phonebook.lookup(name="John Doe")
+def test_phonebook_lookup(phone_book: object):
+
+    phone_book.add(name="John Doe", number="1234567890")
+    number = phone_book.lookup(name="John Doe")
     assert number == "1234567890"
 
-def test_missing_name():
-    new_phonebook: object = phonebook.PhoneBook()
-    new_phonebook.add(name="John Doe", number="1234567890")
+def test_missing_name(phone_book: object):
+    phone_book.add(name="John Doe", number="1234567890")
     with pytest.raises(Exception):
-        assert new_phonebook.lookup(name="Jane")
+        assert phone_book.lookup(name="Jane")
 
-def test_phonebook_consistency():
-    is_consistent: bool = True
+def test_is_consistent(phone_book):
+    assert phone_book.is_consistent() == True
+
+def test_is_not_consistent(phone_book):
+    assert phone_book.is_consistent(consistency = False) == False
+
+def test_phonebook_consistency_duplicate_prefix(phone_book: object):
     people: list = [
         ("f1 l1", "198724"),
         ("f2 l2", "298734"),
@@ -50,13 +57,21 @@ def test_phonebook_consistency():
         ("f4 l4", "298"),
     ]
 
-    new_phonebook: object = phonebook.PhoneBook()
-    new_phonebook.add_multiple(people)
+    phone_book.add_multiple(people)
+    assert phone_book.is_consistent() == True, "No number should be a duplicate of another."
 
-    for i, number in enumerate(new_phonebook.numbers.values()):
-        for j, prefix in enumerate(new_phonebook.numbers.values()):
-            if i != j and prefix in number:
-                is_consistent = False
-                break
+def test_phonebook_consistency_duplicate(phone_book: object):
+    people: list = [
+        ("f1 l1", "198724"),
+        ("f2 l2", "298734"),
+        ("f3 l3", "198724"),
+        ("f4 l4", "298734"),
+    ]
 
-    assert is_consistent == True, "No number should be the prefix of another."
+    phone_book.add_multiple(people)
+    assert phone_book.is_consistent() == True, "No number should be a duplicate of another."
+
+def test_phonebook_consistency_when_empty(phone_book: object):
+    people: list = []
+    phone_book.add_multiple(people)
+    assert phone_book.is_consistent() == True, "No number should be a duplicate of another."
